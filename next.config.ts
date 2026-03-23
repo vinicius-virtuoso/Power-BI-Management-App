5af1cb96534534c3d97f31a8580c3b78551fad82
Vinicius Virtuoso <viniciusvirtuoso1902@gmail.com>
2026-03-23 15:14:45 -0300

    fix: ajuste de cors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
+import "dotenv/config";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: process.env.FRONTEND_URL ?? "https://pbm-vini.vercel.app",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,PUT,PATCH,DELETE",
+          },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
